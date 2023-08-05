aa14f7ddf95eec1922dc2cacd2351c689356e524
Kevan Williams <kevanwilliams@Web3jds-MacBook-Pro.local>
2023-08-05 19:45:50 -0300

    makecopies

--- Python/makecopies.py
+++ Python/makecopies.py
@@ -3,7 +3,6 @@ import shutil
 original=r'../Resources/test_metadata/1.json'
 
 
-
-for x in range(1,101):
+for x in range(2, 101):
     target = r'../Resources/test_metadata/'+str(x)+'.json'
     shutil.copyfile(original, target)
