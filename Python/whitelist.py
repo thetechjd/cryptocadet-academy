bcdae9577c570f3a783698ded35a1e8a63af1206
Kevan Williams <kevanwilliams@Web3jds-MacBook-Pro.local>
2023-08-07 23:01:01 -0300

    simple conversion

--- Python/whitelist.py
+++ Python/whitelist.py
@@ -1,19 +1,19 @@
 import os
 
-
-
 f = open('../Resources/winners.txt', 'r')
 lines = f.readlines()
 
 
 addresses = []
 
-
 for line in lines:
     line = line.rstrip("\n")
-    
+
     if '0x' in line:
         addresses.append(line)
 
 
 print(addresses)
+
+
+
